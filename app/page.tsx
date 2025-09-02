d758e11113f43a7d1a6e1a8a4bc786df340bb5f4
VazhaK14 <khayrivazhasocial@gmail.com>
2025-09-02 22:16:37 +0700

    Finalize Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div>UIIW PROJECT</div>
+      <div className="h-screen">UIIW PROJECT</div>
     </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,32 @@
+"use client";
 import Image from "next/image";
 import { ChevronDown, AlignRight } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+      setLastScrollY(window.scrollY);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
   return (
     <>
-      <div className="relative flex items-center justify-center px-10 py-8">
+      <div
+        className={`fixed top-0 left-0 w-full flex items-center justify-center px-10 py-8 z-50 transition-all duration-500 delay-300 ${
+          isVisible ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
         <nav className="flex flex-wrap w-7xl bg-gradient-to-b from-primary-5 to-primary-4 items-center justify-between max-md:border-2 border-3 border-primary-1 rounded-full max-md:px-8 px-12 py-2">
           <div className="flex flex-wrap items-center gap-3">
             <div className="relative max-md:w-8 max-md:h-8 w-14 h-14">
@@ -21,17 +43,17 @@ const Navbar = () => {
             </p>
           </div>
           <div className="flex max-md:hidden flex-row font-semibold text-md font-visby gap-10 text-primary-1">
-            <p>Home</p>
-            <p>About Us</p>
-            <p className="flex items-center gap-2">
+            <p className="hover:cursor-pointer">Home</p>
+            <p className="hover:cursor-pointer">About Us</p>
+            <p className="flex items-center gap-2 hover:cursor-pointer">
               Programs{" "}
               <span>
                 <ChevronDown />
               </span>
             </p>
-            <p>Register</p>
+            <p className="hover:cursor-pointer">Register</p>
           </div>
-          <AlignRight className="size-7 md:hidden text-primary-1" />
+          <AlignRight className="size-7 md:hidden text-primary-1 " />
         </nav>
       </div>
     </>
